aa6d9fc5435f1adb7d6765ea0699ba1882a7ae2b
Daniel Rose <dan@modiho.com>
2015-11-12 18:56:33 +0100

    Draw character.

--- src/run.js
+++ src/run.js
@@ -1,6 +1,22 @@
 const canvas = document.getElementById('game');
 const width = 1200;
 const height = width/16*9;
+const center = {
+    x: width / 2,
+    y: height / 2,
+};
+const context = canvas.getContext('2d');
 
-canvas.width = width;
-canvas.height = height;
+setCanvasProportions();
+drawCharacter();
+
+function drawCharacter() {
+    context.beginPath();
+    context.arc(center.x, center.y, 20, 0, Math.PI);
+    context.fill();
+}
+
+function setCanvasProportions() {
+    canvas.width = width;
+    canvas.height = height;
+}
